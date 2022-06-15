3feed980889be69e00643541ce4eea482ba61733
toadjiang <12012801@mail.sustech.edu.cn>
2022-06-15 16:01:07 +0800

    6/15 16:00 jy create

---
+++ Matrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jyl on 2022/6/15.
+//
+
+#include "Matrix.h"
---
+++ Matrix.h
@@ -0,0 +1,14 @@
+//
+// Created by jy on 2022/6/15.
+//
+
+#ifndef MATRIX_MATRIX_H
+#define MATRIX_MATRIX_H
+template <typename T>
+class Mat{
+public:
+    int col;
+    int row;
+
+};
+#endif //MATRIX_MATRIX_H
