dae8d88a95bccfc1f54c161d6a83bdb22e2b88d3
toadjiang <12012801@mail.sustech.edu.cn>
2022-06-15 17:30:15 +0800

    6/15 17:30 jy mat get set complete

--- Matrix.h
+++ Matrix.h
@@ -2,20 +2,11 @@
 #define MATRIX_MATRIX_H
 
 #include <unordered_map>
-class Point{
-public:
-    int x;
-    int y;
-    Point(int x, int y){
-        this->x = x;
-        this->y = y;
-    }
-};
 
 class Mat{
 public:
-    int col;
     int row;
+    int col;
     int step;
     int numOfElements;
     std::unordered_map<int,int> Map;
