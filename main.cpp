0ded5cfa6f535db35c8aae80f23aad2ca5f6dd31
toadjiang <12012801@mail.sustech.edu.cn>
2022-06-18 19:24:30 +0800

    6/18 19:20 jy

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int main() {
-    vector<int> l1{1, 2, 1, 0, 0, 0, -1, -2, -1};
-    Mat<int> mat1(3, 3, &l1, false);
+    vector<int> l1{1, 2, 1, 0, 0, 0};
+    Mat<int> mat1(3, 2, &l1, false);
     vector<int> l2{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
     Mat<int> mat2(5, 4, &l2, false);
     cout << "mat1" << endl;
